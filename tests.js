4363eb1310a8868259548e0545b1f5da35575c9f
Moriah Humphries <moriah.humphries@gmail.com>
2020-03-23 16:58:23 -0500

    working on exercise

--- tests.js
+++ tests.js
@@ -33,4 +33,9 @@ describe('sayHello', function () {
         expect(sayHello(" Pat")).toBe("Hello, Pat!");
     });
 
+    it('should return the string "Hello, World!" when executed', function() {
+        expect(sayHello(" World")).toBe("Hello, World!");
+    });
+
+
 })
